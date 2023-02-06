cf1ec055642d932be877c1511a2e66589d9e4c9d
zhangzhongyou <zhangzhongyou@bytedance.com>
2023-02-06 23:03:23 +0800

    feat: 初始化项目

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+}
